6f40c434de329f16e24086be8e8db0c0dde79342
davidcshepherd <davidshepherd@gmail.com>
2020-03-26 11:50:03 -0400

    Updated notes

---
+++ Day 3/notesForDemo2.rb
@@ -0,0 +1,15 @@
+##Start
+##Using D minor chord
+##scale: D, E, F, G, A, B?, and C
+##chord: D, F, A 
+use_bpm 96
+play chord(:D3, :minor), release: 8
+##melody
+play :D5
+sleep 1
+play :E5
+sleep 1
+play :F5
+sleep 1
+play :G5
+sleep 1
