d08ce82a7c381e29fd89546db7bc271702227341
davidcshepherd <davidshepherd@gmail.com>
2020-03-26 11:10:50 -0400

    Create notesForDemo.rb

---
+++ Day 3/notesForDemo.rb
@@ -0,0 +1,81 @@
+##Start
+use_bpm 96
+play :eb5
+sleep 1
+play :bb5
+sleep 1
+play :bb5
+sleep 1
+play :ab5
+sleep 1
+play :bb5
+sleep 1
+play :ab5
+sleep 1
+play :gb5
+sleep 1
+play :ab5
+sleep 1
+play :ab5
+sleep 1
+play :gb5
+sleep 1
+play :eb5
+sleep 1
+play :gb5
+sleep 1
+
+
+##2
+use_bpm 96
+play :eb5
+sleep 1
+play :bb5
+sleep 1
+play :bb5
+sleep 0.5
+play :ab5
+sleep 0.5
+play :bb5
+sleep 1
+play :ab5
+sleep 0.5
+play :gb5
+sleep 0.5
+play :ab5
+sleep 1
+play :ab5
+sleep 0.5
+play :gb5
+sleep 0.5
+play :eb5
+sleep 0.5
+play :gb5
+sleep 0.5
+
+##3
+use_bpm 96
+play :eb5, release: 0.2
+sleep 1
+play :bb5, release: 0.2
+sleep 1
+play :bb5, release: 0.2
+sleep 0.5
+play :ab5, release: 0.2
+sleep 0.5
+play :bb5, release: 0.2
+sleep 1
+play :ab5, release: 0.2
+sleep 0.5
+play :gb5, release: 0.2
+sleep 0.5
+play :ab5, release: 0.2
+sleep 1
+play :ab5, release: 0.2
+sleep 0.5
+play :gb5, release: 0.2
+sleep 0.5
+play :eb5, release: 0.2
+sleep 0.5
+play :gb5, release: 0.2
+sleep 0.5
