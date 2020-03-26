781d1a158289169c5344cf391ec4acb785f6daa9
davidcshepherd <davidshepherd@gmail.com>
2020-03-26 15:01:35 -0400

    Update 2Lesson-Rhythm-OldTown.rb

--- Day 2/2Lesson-Rhythm-OldTown.rb
+++ Day 2/2Lesson-Rhythm-OldTown.rb
@@ -26,6 +26,66 @@ live_loop :melody, sync: :start do
 end
 
 # Please, do not change the code below, it is the background song for your activity :D
+
+
+#Time
+define :resetTime do
+  set :beat, 1
+  set :bar, 1
+end
+
+define :getBar do
+  get[:bar]
+end
+
+define :getBeat do
+  get[:beat]
+end
+
+define :setBar do |a|
+  set :bar, a
+end
+
+define :setBeat do |a|
+  set :beat, a
+end
+
+define :setBnB do |bar, beat|
+  set :bar, bar
+  set :beat, beat
+end
+
+define :time do
+  if (getBeat < 4)
+    setBeat (getBeat + 1)
+  else
+    setBeat 1
+    setBar (getBar + 1)
+  end
+end
+
+define :looper do |loopBegin, loopEnd|
+  if (getBar == loopEnd)  # Loop
+    setBar loopBegin
+  end
+end
+
+define :sequencer do |cueName, length, startBar, startBeat, endBar, endBeat|
+  
+  if (getBar >= startBar && getBar <= endBar)
+    if ((getBar - startBar) % length == 0)
+      if (getBeat == startBeat)
+        cue cueName
+      end
+    end
+  end
+  
+end
+
+define :counter do
+  print "bar: " + getBar.to_s + " beat: " + getBeat.to_s
+end
+
 set_mixer_control! amp: 7
 
 set :bassNKickVol, 1
@@ -316,62 +376,4 @@ live_loop :conductor, delay: 1.0 do
   
   time
   
-end
-
-#Time
-define :resetTime do
-  set :beat, 1
-  set :bar, 1
-end
-
-define :getBar do
-  get[:bar]
-end
-
-define :getBeat do
-  get[:beat]
-end
-
-define :setBar do |a|
-  set :bar, a
-end
-
-define :setBeat do |a|
-  set :beat, a
-end
-
-define :setBnB do |bar, beat|
-  set :bar, bar
-  set :beat, beat
-end
-
-define :time do
-  if (getBeat < 4)
-    setBeat (getBeat + 1)
-  else
-    setBeat 1
-    setBar (getBar + 1)
-  end
-end
-
-define :looper do |loopBegin, loopEnd|
-  if (getBar == loopEnd)  # Loop
-    setBar loopBegin
-  end
-end
-
-define :sequencer do |cueName, length, startBar, startBeat, endBar, endBeat|
-  
-  if (getBar >= startBar && getBar <= endBar)
-    if ((getBar - startBar) % length == 0)
-      if (getBeat == startBeat)
-        cue cueName
-      end
-    end
-  end
-  
-end
-
-define :counter do
-  print "bar: " + getBar.to_s + " beat: " + getBeat.to_s
 end
