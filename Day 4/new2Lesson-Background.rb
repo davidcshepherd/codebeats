70bdf772ad528d91e8e0431edaeefe2de1fcc8b5
davidcshepherd <davidshepherd@gmail.com>
2020-04-30 16:25:55 -0400

    Update new2Lesson-Background.rb

--- Day 4/new2Lesson-Background.rb
+++ Day 4/new2Lesson-Background.rb
@@ -13,16 +13,33 @@ use_bpm 90
 live_loop :drum1 do
   # you can control the amp for a group of samples
   use_sample_defaults amp: 1
-  # You can insert your samples here
-  sleep 4
+  # You can alter the code below by:
+  # -Changing the sleep values
+  # -Changing the sample
+  # -Adding more samples
+  # -Adding more sleeps
+  # Emphasize beat #1 in this loop.
+  sample :drum_heavy_kick
+  sleep 2.5
+  sample :drum_heavy_kick
+  sleep 1.5
 end
 
 # A different live_loop - using another sample
 # You can use different samples at the same live_loop too
 live_loop :drum2 do
   use_sample_defaults amp: 1
-  # You can insert your samples here
-  sleep 4
+  # You can alter the code below by:
+  # -Changing the sleep values
+  # -Changing the sample
+  # -Adding more samples
+  # -Adding more sleeps
+  # Emphasize beats #2 and #4 in this loop.
+  sleep 1
+  sample :sn_dolf
+  sleep 2
+  sample :sn_dolf
+  sleep 1
 end
 
 live_loop :add_sample1 do
