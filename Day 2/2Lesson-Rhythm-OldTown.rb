0195399cd11caaaaedca744df1322b7ff7a611e1
davidcshepherd <davidshepherd@gmail.com>
2020-05-04 21:35:35 -0400

    Updated submission URL

--- Day 2/2Lesson-Rhythm-OldTown.rb
+++ Day 2/2Lesson-Rhythm-OldTown.rb
@@ -8,7 +8,7 @@ live_loop :melody, sync: :start do
     #
     #  Tip: the sleeps must add up to 8.0 in total (or multiples of 8).
     #  Tip: if the note is too long make the sustain shorter than 1.
-    #  Tip: upload to https://bit.ly/submitBeats when done!
+    #  Tip: upload to https://forms.gle/ThAM3PWDg3oQZmuJ9 when done!
     ##################################
     sample :drum_cowbell
     sleep 0.25
