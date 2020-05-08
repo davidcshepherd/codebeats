b60cff3254b2c8f00edf497a0d961890d10c698f
Douglas Krug <60158593+dougkrug@users.noreply.github.com>
2020-05-08 16:49:02 -0400

    Create 2Lesson-Loops_Chords.rb

---
+++ Week 2/Day 4/2Lesson-Loops_Chords.rb
@@ -0,0 +1,38 @@
+# Set the bpm as you wish
+use_bpm 120
+
+##### Ninth activity #####
+#  Use the structure of live_loops to create your song
+#  create your drums part, your chords part and your melody
+#  don't forget to care of the amount of sleeps in each live_loop.
+#  It would be nice to have the same amount or,
+#  at least, a value that is multiple of 4 (beats).
+#
+#  The main challenge is to continue modifying your song
+#  without press "stop" and "run" again, just press
+#  "run" after your changes to check how it is!
+#
+#  Tip: Try one live_loop at each time!
+#  Tip: upload to https://forms.gle/ThAM3PWDg3oQZmuJ9 when done!
+##########################
+
+live_loop :drums do
+  use_sample_defaults amp: 1
+  use_synth_defaults amp: 1
+  # Drum's live_loop: insert your code here:
+  sleep 4
+end
+
+live_loop :chords do
+  use_sample_defaults amp: 1
+  use_synth_defaults amp: 1
+  # Chord's live_loop: insert your code here:
+  sleep 4
+end
+
+live_loop :melody do
+  use_sample_defaults amp: 1
+  use_synth_defaults amp: 1
+  # Melody's live_loop: insert your code here:
+  sleep 4
+end
